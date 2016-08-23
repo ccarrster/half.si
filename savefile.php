3a1a1b42520bbff2c4e600394918f33b692709ef
ccarr <ccarrster@gmail.com>
2016-08-23 00:34:17 -0400

    Saving images

---
+++ savefile.php
@@ -0,0 +1,32 @@
+<?php
+// You need to add server side validation and better error handling here
+include_once('./lib/QrReader.php');
+$data = array();
+$fileName = '';
+if(isset($_FILES))
+{  
+    $error = false;
+    $files = array();
+
+    $uploaddir = './uploads/';
+    foreach($_FILES as $file)
+    {
+        if(move_uploaded_file($file['tmp_name'], $uploaddir .basename($file['name'])))
+        {
+            $files[] = $uploaddir .$file['name'];
+            $fileName = $uploaddir .$file['name'];
+        }
+        else
+        {
+            $error = true;
+        }
+    }
+    $data = ($error) ? array('error' => 'There was an error uploading your files') : array('files' => $files);
+}
+else
+{
+    $data = array('success' => 'Form was submitted', 'formData' => $_POST);
+}
+echo exec('zbarimg '.$fileName);
+echo json_encode($data);
+?>
